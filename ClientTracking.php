fbf86bdab27ee0f5df56526e5a31117b88c488ab
zensic <anthonyleehj@gmail.com>
2020-11-03 21:58:10 +0800

    Added TrackingPage implementation

--- ClientTracking.php
+++ ClientTracking.php
@@ -14,38 +14,94 @@
 <body>
     <?php
         include 'include/ClientsNavBar.php';
-        include "backend/DatabaseConnect.php";
-        $db = new mysqli($SERVERNAME, $USERNAME, $PASSWORD, $DATABASE);
-        $sql = "SELECT * FROM Orders WHERE OrderID='1'";
-        $result = $db->query($sql);
-        $row = $result->fetch_assoc();
     ?>
-    <div class="container">
-        <div class="jumbotron progressBar">
-            <h3>Order Status</h3>
-            <ul>
-                <li <?php if($row["TrackingID"] > 0){echo "class='selected'";} ?>>
-                    <span>Step 1</span>
-                    <div>Invoice Issued</div>
-                </li>
-                <li <?php if($row["TrackingID"] > 1){echo "class='selected'";} ?>>
-                    <span>Step 2</span>
-                    <div>Order Confirmed</div>
-                </li>
-                <li <?php if($row["TrackingID"] > 2){echo "class='selected'";} ?>>
-                    <span>Step 3</span>
-                    <div>Event Preparing</div>
-                </li>
-                <li <?php if($row["TrackingID"] > 3){echo "class='selected'";} ?>>
-                    <span>Step 4</span>
-                    <div>Event Preparation Completed</div>
-                </li>
-                <li <?php if($row["TrackingID"] > 4){echo "class='selected'";} ?>>
-                    <span>Step 5</span>
-                    <div>Event completed</div>
-                </li>
-            </ul>
-        </div>
-    </div>
+    <section class="container order">
+        <?php
+            $clientID = 'CL00000001'; // change to session value later
+
+            // connect to db
+            include "backend/DatabaseConnect.php";
+            $db = new mysqli($SERVERNAME, $USERNAME, $PASSWORD, $DATABASE);
+
+            // fetches all data from the tables
+            $sql = 'SELECT * FROM orders INNER JOIN catering_package ON orders.PackageID=catering_package.PackageID WHERE ClientID="'.$clientID.'";';
+            $result = $db->query($sql);
+            
+            // print data a few times
+            if ($result->num_rows > 0) {
+                while ($row = $result->fetch_assoc()) {
+            
+                    $tracking1 = "";
+                    $tracking2 = "";
+                    $tracking3 = "";
+                    $tracking4 = "";
+                    $tracking5 = "";
+                    $totalPrice = $row["PricePerPax"]*$row["NumPeople"];
+
+                    if ($row["TrackingID"] > 4) {
+                        $tracking1 = " selected";
+                        $tracking2 = " selected";
+                        $tracking3 = " selected";
+                        $tracking4 = " selected";
+                        $tracking5 = " selected";
+                    }
+                    else if ($row["TrackingID"] > 3) {
+                        $tracking1 = " selected";
+                        $tracking2 = " selected";
+                        $tracking3 = " selected";
+                        $tracking4 = " selected";
+                    }
+                    else if ($row["TrackingID"] > 2) {
+                        $tracking1 = " selected";
+                        $tracking2 = " selected";
+                        $tracking3 = " selected";
+                    }
+                    else if ($row["TrackingID"] > 1) {
+                        $tracking1 = " selected";
+                        $tracking2 = " selected";
+                    }
+                    else if ($row["TrackingID"] > 0) {
+                        $tracking1 = " selected";
+                    }
+                    
+                    echo '
+                    <div class="jumbotron">
+                        <div class="row" style="cursor: pointer;">
+                            <div class="col-md-2 col-sm-4">
+                                <img class="img-fluid rounded img-thumbnail" src="'.$row["ImagePath"].'">
+                            </div>
+                            <div class="col-md-10 col-sm-8">
+                                <h3>'.$row["PackageName"].'</h3>
+                                <h5>ID: '.$row["OrderID"].'</h5>
+                                <h5>Date Received: '.$row["OrderDate"].'</h5>
+                                <h5>Price: RM '.$totalPrice.'</h5>
+                            </div>
+                        </div>
+                        <hr/>
+                        <div>
+                        <div class="row">
+                            <div class="col-sm bold'.$tracking1.'">
+                                Invoice Issued
+                            </div>
+                            <div class="col-sm bold'.$tracking2.'">
+                                Order Confirmed
+                            </div>
+                            <div class="col-sm bold'.$tracking3.'">
+                                Event Preparing
+                            </div>
+                            <div class="col-sm bold'.$tracking4.'">
+                                Event Preparation
+                            </div>
+                            <div class="col-sm bold'.$tracking5.'">
+                                Event Completed
+                            </div>
+                        </div>
+                        </div>
+                    </div>
+                    ';
+                }
+            }
+        ?>
+    </section>
 </body>
 </html>
