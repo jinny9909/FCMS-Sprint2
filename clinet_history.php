905eddbc1b234484ae86d489c7d3da33d7df845a
simonlu97 <62094893+simonlu97@users.noreply.github.com>
2020-11-06 21:11:22 +0800

    Update clinet_history.php

--- clinet_history.php
+++ clinet_history.php
@@ -11,44 +11,74 @@
 			include 'include/NavBarStyle.php';
 		?>
 		<style>
-			
+			table{
+				margin-top: 100px;
+			}
+			td{
+				background-color: white;
+				text-align: center;
+			}
 		</style>
 	</head>
 
 	<body>
 		<?php
-			include 'include/OTNavBar.php';
+			include 'include/ClientsNavBar.php';
 		?>
-		<div class="container">
-			<div class="col">
-			<?php
-				// Create database connection
-				$db = mysqli_connect("localhost", "root", "", "fcms");
-				//Uncomment this section to check database connection
-				if ($db) {
-					echo "Successful Connect to DB<br/>";
-				} else {
-					die("fail");
-				}
-				$sql = "SELECT order_ID, order_date, package_ID, total_price from client_history";
-				$result=$db-> query($sql);
+		<div class="container inner-ontainer tab_box">
+			<div class="row clearfix">
+					<div class="col-md-12 table-responsive">
+						<table class="table table-bordered table-hover table-sortable" id="tab_logic">
+							<thead class="table_heading">
+								<tr >
+									<th class="text-center">
+										Order ID
+									</th>
+									<th class="text-center">
+										 Order Date
+									</th>
+									<th class="text-center">
+										Package ID
+									</th>
+									<th class="text-center" >
+										Total Price
+									</th>
+									<th class="text-center" >
+										Number of Diner
+									</th>
+								</tr>
+							</thead>
+							<?php
+							// Create database connection
+							$db = mysqli_connect("localhost", "root", "", "fcms");
+							//Uncomment this section to check database connection
+							if ($db) {
+								echo "Successful Connect to DB<br/>";
+							} else {
+								die("fail");
+							}
+							$sql = "SELECT order_ID, order_date, package_ID, total_price, num_diner from client_history";
+							$result=$db-> query($sql);
 							
-				if ($result-> num_rows >0){
-					while ($row = $result-> fetch_assoc()){
-						echo 
-						"<div>
-							<p>Order ID: ".$row["order_ID"]."</p>
-							<p>Order Date: ".$row["order_date"]."</p>
-							<p>Package ID: ".$row["package_ID"]."</p>
-							<p>Total Price: ".$row["total_price"]."</p>
-						</div>";
-						}
-					}
-					else{
-						echo "0 result";
-					}
-					$db-> close();
-			?>
+							if ($result-> num_rows >0){
+								while ($row = $result-> fetch_assoc()){
+									echo 
+									"<tr>
+										<td>".$row["order_ID"]."</td>
+										<td>".$row["order_date"]."</td>
+										<td>".$row["package_ID"]."</td>
+										<td>".$row["total_price"]."</td>
+										<td>".$row["num_diner"]."</td>
+									</tr>";
+								}
+							}
+							else{
+								echo "0 result";
+							}
+							$db-> close();
+							?>
+					</table>
+				</div>
 			</div>
 		</div>
 	</body>
