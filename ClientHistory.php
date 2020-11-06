b31f70e6d2456ff4f037368ca6ad054cc770b65a
simonlu97 <62094893+simonlu97@users.noreply.github.com>
2020-11-06 21:20:54 +0800

    Update ClientHistory.php

--- ClientHistory.php
+++ ClientHistory.php
@@ -2,7 +2,7 @@
 <html>
 	<head>
 	
-		<title>[OT] Update Order Page</title> 
+		<title>Client History</title> 
 		<meta charset="utf-8">
 		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 		<link rel="stylesheet" href="styles/OTUpdateOrder.css">
